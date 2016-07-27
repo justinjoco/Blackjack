c6eff119422d350f10f2359e67dbd7b9aefaaeaa
justinjoco <justin.joco@gmail.com>
2016-07-27 08:46:34 -0400

    Update rightTriangle.java

--- shapes/rightTriangle.java
+++ shapes/rightTriangle.java
@@ -5,17 +5,17 @@ public class rightTriangle extends Shape{
 	rightTriangle(double x, double y, double z) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
-		this.base=super.x;
-		this.height=super.y;
+		this.legA=super.x;
+		this.legB=super.y;
 		this.hypotenuse=z;
 	}
-	private double base,height,hypotenuse;
+	private double legA,legB,hypotenuse;
 	double getArea(){
-		return (base*height)/2;
+		return (legA*legB)/2;
 	}
 	
 	double getPerimeter(){
-	return base+height+hypotenuse;
+	return legA+legB+hypotenuse;
 	}
 
 
